468c93dcd37ee2db2b143f5217f6f4499cf73277
kspu0822INST377 <kspu@terpmail.umd.edu>
2020-03-11 22:14:03 -0400

    finish

--- lab_5/scripts/main.js
+++ lab_5/scripts/main.js
@@ -1,1 +1,20 @@
+document.getElementById("extraCredit").addEventListener("click", setCountry);
+
+function setCountry() {
+    const content = document.querySelector(".content");
+    middle = ""
+    countryList = new Array(25);
+
+    for(let step = 0; step < 25; step++)
+    {
+        do {
+            currentValue = countries[Math.floor(Math.random() * countries.length)];
+          }
+        while (countryList.includes(currentValue));
+        countryList[step]= currentValue;
+        middle += "<li>" + "<b>" + countryList[step].code +"</b>" + " " + countryList[step].name + "</li>";
+    }
+    content.innerHTML = "<ol class = \"country\">" + middle + "<\ol>";
+  }
+
 
