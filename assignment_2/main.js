9dd3956a835617ccfa2b2fd7200b9e7019255701
kspu0822INST377 <kspu@terpmail.umd.edu>
2020-04-02 11:45:08 -0400

    finished

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -2,12 +2,39 @@ const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
 const btn = document.querySelector('button');
+btn.addEventListener('click', shadeChange);
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+for(var current = 1 ; current <= 5 ; ++current)
+{
+    var targetImage = document.createElement('img');
+    targetImage.setAttribute('src', 'images/pic' + current + '.jpg');
+    thumbBar.appendChild(targetImage);
+    
+    targetImage.addEventListener('click', zoomImage);
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
-
+function zoomImage(tarImage)
+{
+    var tarPth = tarImage.target.getAttribute('src');
+    displayedImage.setAttribute('src', tarPth);
+}
 /* Wiring up the Darken/Lighten button */
+
+function shadeChange()
+{
+    var state = btn.getAttribute('class');
+    if(state === 'dark')
+    {
+        btn.innerText = 'Lighten';
+        btn.setAttribute('class', 'light');
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    }
+    else
+    {
+        btn.innerText = 'Darken';
+        btn.setAttribute('class', 'dark');
+        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+}
